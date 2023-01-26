ec48dbab04109a9a64b42d19349cf2e87d4de97a
Sky Cope <skycope@gmail.com>
2023-01-26 16:05:04 +0200

    first commit. making a simple react app that will eventually interface with some OpenAI APIs to do something

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { animated, useTransition } from 'react-spring';
+import { CSSTransition } from 'react-transition-group';
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  input {
+    width: 50%;
+    padding: 20px;
+    font-size: 15px;
+    border-radius: 15px;
+    border: 1px solid #000000;
+    outline: none;
+  }
+`;
+
+<CSSTransition
+  in={outputBoxVisible}
+  timeout={300}
+  classNames="output-box"
+  unmountOnExit
+>
+  <OutputBox>{displayValue}</OutputBox>
+</CSSTransition>
+
+const OutputBox = styled(animated.div)`
+  background: #ffffff;
+  padding: 20px;
+  margin-top: 20px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 10px #cccccc;
+`;
+
+const [outputBoxVisible, setOutputBoxVisible] = useState(false);
+
+const StyledButton = styled.button`
+  background: #000000;
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+  &:hover{
+    background: #555555;
+  }
+`;
+
+const [displayValue, setDisplayValue] = useState('');
+const [isOutputVisible, setIsOutputVisible] = useState(false);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  setIsOutputVisible(true);
+  setDisplayValue(inputValue);
+  setInputValue('');
 }
 
+// between input box and submit button, add 5px of horizontal margin
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [displayValue, setDisplayValue] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setDisplayValue(inputValue);
+    setInputValue('');
+  }
+
+
+
+return (
+    <InputContainer>
+    <div className="heading">TEST</div>
+    <div className="center">
+        <form onSubmit={handleSubmit}>
+          <input
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+          <StyledButton type="submit">Submit</StyledButton>
+        </form>
+        {isOutputVisible && (
+          <OutputBox className="output-box">
+            {displayValue}
+          </OutputBox>
+        )}
+    </div>
+    </InputContainer>
+);
+
+
+}
+
+
 export default App;
+
