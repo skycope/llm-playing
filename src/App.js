ae670ae7ec1b04007ccc5960c5ef76c317b4669a
Sky Cope <skycope@gmail.com>
2023-01-27 14:59:51 +0200

    major changes. decided to make it a colour app.

--- src/App.js
+++ src/App.js
@@ -1,77 +1,63 @@
 import React, { useState } from 'react';
-import { animated, useTransition } from 'react-spring';
-import { CSSTransition } from 'react-transition-group';
-import styled from 'styled-components';
 import './App.css';
 
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
 
-  input {
-    width: 50%;
-    padding: 20px;
-    font-size: 15px;
-    border-radius: 15px;
-    border: 1px solid #000000;
-    outline: none;
-  }
-`;
+// onChange is a prop that is passed to the SplitButton component
+// It is a function that is called when the input value changes
 
-const OutputBox = styled(animated.div)`
-  background: #ffffff;
-  padding: 20px;
-  margin-top: 20px;
-  border-radius: 10px;
-  box-shadow: 2px 2px 10px #cccccc;
-`;
+function onChange(event) {
+  console.log(event.target.value);
+}
 
-const StyledButton = styled.button`
-  background: #000000;
-  color: #fff;
-  padding: 10px 20px;
-  border-radius: 10px;
-  border: none;
-  outline: none;
-  margin-left: 5px;
-  &:hover {
-    background: #555555;
-  }
-`;
+
+const SplitButton = ({ onClick, onKeyDown }) => {
+  return (
+    <div className="split-button">
+      <input className="split-button__input" type="text" onChange={onChange} onKeyDown={onKeyDown} />
+      <button className="split-button__submit" onClick={onClick}>Submit</button>
+    </div>
+  );
+}
+
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [displayValue, setDisplayValue] = useState('');
-  const [isOutputVisible, setIsOutputVisible] = useState(false);
+  const [backgroundColor, setBackgroundColor] = useState('');
+  const [setIsInputFilled] = useState(false);
 
-  function handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    setIsOutputVisible(true);
-    setDisplayValue(inputValue);
-    setInputValue('');
+    const randomColor = getRandomColor();
+    setBackgroundColor(randomColor);
+  }
+
+  const handleKeyDown = (event) => {
+    if(event.key === 'Enter') {
+      handleSubmit(event);
+    }
+  }
+
+  const handleInput = (event) => {
+    if (event.target.value) {
+      setIsInputFilled(true);
+    } else {
+      setIsInputFilled(false);
+    }
+  }
+
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 
   return (
-    <InputContainer>
-      <div className="heading">TEST</div>
-      <div className="center">
-        <form onSubmit={handleSubmit}>
-          <input
-            value={inputValue}
-            onChange={(event) => setInputValue(event.target.value)}
-          />
-          <StyledButton type="submit">Submit</StyledButton>
-        </form>
-        {isOutputVisible && (
-          <OutputBox className="output-box">
-            {displayValue}
-          </OutputBox>
-        )}
-      </div>
-    </InputContainer>
+    <div className="App center" style={{ backgroundColor, height: '100vh', width: '100vw' }}>
+      <h1 className="heading">How are you feeling?</h1>
+      <SplitButton onClick={handleSubmit} onKeyDown={handleKeyDown} onInput={handleInput} />
+    </div>
   );
 }
 
